d07a6da814efa400dc67712d8eaef851fd44562a
jzcurious <paranoid.hpc.io@gmail.com>
2024-10-15 12:04:36 +0700

    add ipython

--- run.sh
+++ run.sh
@@ -4,3 +4,5 @@ CUI_ROOT=/content/drive/MyDrive/cui
 WORKDIR="$CUI_ROOT/ComfyUI"
 
 python "$WORKDIR/main.py" --dont-print-server
+
+# python /content/drive/MyDrive/cui/ComfyUI/main.py --dont-print-server
